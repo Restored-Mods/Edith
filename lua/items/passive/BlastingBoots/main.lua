6de71077c6386a851d83c2539d0cd210f4862f3b
John sorrow <yfwoaao@gmail.com>
2025-03-10 09:30:31 -0400

    Blasting Boots Holy Mantle fix

--- lua/items/passive/BlastingBoots/main.lua
+++ lua/items/passive/BlastingBoots/main.lua
@@ -1,26 +1,17 @@
 local BlastBoots = {}
 
----@param e Entity
----@param amount integer
+---@param player EntityPlayer
 ---@param flags DamageFlag | integer
----@param source EntityRef
----@param countdown integer
----@return boolean
-function BlastBoots:OnBombDamage(e, amount, flags, source, countdown)
-	if e and e:ToPlayer() then
-        local player = e:ToPlayer()
-        if not player:HasCollectible(EdithRestored.Enums.CollectibleType.COLLECTIBLE_BLASTING_BOOTS) then
-            return
-        end
-		if flags & DamageFlag.DAMAGE_EXPLOSION == DamageFlag.DAMAGE_EXPLOSION then
-            JumpLib:Jump(player, {
-                Height = 13,
-                Speed = 1,
-                Flags = JumpLib.Flags.NO_HURT_PITFALL | JumpLib.Flags.FAMILIAR_FOLLOW_ORBITALS_ONLY | JumpLib.Flags.FAMILIAR_FOLLOW_TEARCOPYING_ONLY,
-                Tags = {"BlastingBootsJump"}
-            })
-			return false
-		end
-	end
+function BlastBoots:PreBombDamage(player, _, flags)
+    if flags & DamageFlag.DAMAGE_EXPLOSION == 0 or not player:HasCollectible(EdithRestored.Enums.CollectibleType.COLLECTIBLE_BLASTING_BOOTS) then return end
+
+    JumpLib:Jump(player, {
+        Height = 13,
+        Speed = 1,
+        Flags = JumpLib.Flags.NO_HURT_PITFALL | JumpLib.Flags.FAMILIAR_FOLLOW_ORBITALS_ONLY | JumpLib.Flags.FAMILIAR_FOLLOW_TEARCOPYING_ONLY,
+        Tags = {"BlastingBootsJump"}
+    })
+
+    return false
 end
-EdithRestored:AddCallback(ModCallbacks.MC_ENTITY_TAKE_DMG, BlastBoots.OnBombDamage, EntityType.ENTITY_PLAYER)
+EdithRestored:AddCallback(ModCallbacks.MC_PRE_PLAYER_TAKE_DMG, BlastBoots.PreBombDamage)
