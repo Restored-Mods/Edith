579f81e72046cc7ac4755cce1cf9d28c11227ea2
BrakeDude <lockbrake@gmail.com>
2025-03-13 23:47:22 +0300

    Salt creep adjustments
    - Using Salt Shaker with existing salt creep will affect only ones created by same Salt Shaker user
    - Creep gfx is tied to its InitSeed
    - Using REPENTOGON's RandomInt(min, max)

--- lua/items/active/SaltShaker/main.lua
+++ lua/items/active/SaltShaker/main.lua
@@ -9,27 +9,31 @@ local spawnDegree = 360 / SaltQuantity
 
 local sfx = SFXManager()
 
+---@param collectible CollectibleType | integer
 ---@param rng RNG
 ---@param player EntityPlayer
 ---@param flags UseFlag | integer
-function SaltShakerLocal:UseShaker(_, rng, player, flags)
-    local CarBatteryUse = (flags == flags | UseFlag.USE_CARBATTERY)	
+---@param slot ActiveSlot | integer
+---@param customVarData integer
+---@return table | boolean?
+function SaltShakerLocal:UseShaker(collectible, rng, player, flags, slot, customVarData)
+    local CarBatteryUse = (flags == flags | UseFlag.USE_CARBATTERY)
     if CarBatteryUse then return end
 
-    local hasCarBattery = player:HasCollectible(CollectibleType.COLLECTIBLE_CAR_BATTERY)
-    local SaltTimeOut = hasCarBattery and 480 or 240
+    local SaltTimeOut = player:HasCollectible(CollectibleType.COLLECTIBLE_CAR_BATTERY) and 480 or 240
     local ExistintSalt = Isaac.FindByType(EntityType.ENTITY_EFFECT, SaltCreepVar, SaltCreepSubtype)
 
     for _, ExistSalt in ipairs(ExistintSalt) do
-        ExistSalt:ToEffect():SetTimeout(1)
+        if ExistSalt.SpawnerEntity and GetPtrHash(ExistSalt.SpawnerEntity) == GetPtrHash(player) then
+            ExistSalt:ToEffect():SetTimeout(1)
+        end
     end
 
     for i = 1, SaltQuantity do
         local salt = Isaac.Spawn(EntityType.ENTITY_EFFECT, SaltCreepVar, SaltCreepSubtype, player.Position + Vector.FromAngle(spawnDegree * i) * 60, Vector.Zero, player):ToEffect()
-
         if not salt then return end
 
-        for _ = 1, math.min(2, rng:RandomInt(5)) do
+        for _ = 1, rng:RandomInt(2, 5) do
             Isaac.Spawn(1000, EffectVariant.TOOTH_PARTICLE, 0, salt.Position, RandomVector() * rng:RandomFloat() * rng:RandomInt(6), player):ToEffect()
         end
 
@@ -43,12 +47,12 @@ function SaltShakerLocal:UseShaker(_, rng, player, flags)
 end
 EdithRestored:AddCallback(ModCallbacks.MC_USE_ITEM, SaltShakerLocal.UseShaker, EdithRestored.Enums.CollectibleType.COLLECTIBLE_SALT_SHAKER)
 
-local rng = RNG()
-
+---@param creep EntityEffect
 function SaltShakerLocal:SaltInit(creep)
     local sprite = creep:GetSprite()
-    local randomNum = tostring(rng:RandomInt(6) + 1)
-    sprite:Play("SmallBlood0" .. randomNum, true)  
+    local rng = RNG(creep.InitSeed)
+    local randomNum = tostring(rng:RandomInt(1, 6))
+    sprite:Play("SmallBlood0" .. randomNum, true)
 end
 EdithRestored:AddCallback(ModCallbacks.MC_POST_EFFECT_INIT, SaltShakerLocal.SaltInit, SaltCreepVar)
 
