5830ecd541a897aee010543ae2c28d5f7dcf25c7
BrakeDude <lockbrake@gmail.com>
2025-11-06 09:28:37 +0300

    Error message without REPENTOGON

--- main.lua
+++ main.lua
@@ -1,5 +1,53 @@
 EdithRestored = RegisterMod("Edith", 1)
 
+--#region mod error message, thanks Mr. Fly6
+local isRepentance, isRepentogon = REPENTANCE_PLUS or REPENTANCE, (REPENTOGON or _G._VERSION == "Lua 5.4")
+local errMessage = ""
+local modName = EdithRestored.Name
+local function GetScreenSize()
+	return Vector(Isaac.GetScreenWidth(), Isaac.GetScreenHeight())
+end
+
+if not isRepentance or not isRepentogon then
+	if not isRepentance then
+		errMessage = modName .. " " .. "mod requires Repentance DLC to work"
+	elseif not isRepentogon then
+		errMessage = modName
+			.. " "
+			.. "mod requires Repentogon Script Extender to work. Head to https://repentogon.com/"
+	end
+
+	local transparency = 0
+	EdithRestored:AddCallback(ModCallbacks.MC_POST_RENDER, function()
+		if transparency > 0 then
+			transparency = transparency - 1
+			local a = transparency / 120
+			if a > 1 then
+				a = 1
+			end
+			local screenSize = GetScreenSize()
+			Isaac.RenderScaledText(errMessage, screenSize.X / 5, screenSize.Y / 3, 0.5, 0.5, 1, 0, 0, a)
+		end
+	end)
+
+	EdithRestored:AddCallback(ModCallbacks.MC_POST_GAME_STARTED, function(_, continue)
+		if not continue then
+			transparency = 600
+		end
+	end)
+
+	local eType = Isaac.GetPlayerTypeByName("Redith", false)
+
+    EdithRestored:AddCallback(ModCallbacks.MC_POST_PLAYER_UPDATE, function(_, player)
+        if player:GetPlayerType() == eType then
+			player:ChangePlayerType(PlayerType.PLAYER_ISAAC)
+		end
+    end)
+    
+	return
+end
+--#endregion
+
 local LOCAL_TSIL = require("lua.extraLibs.loi.TSIL")
 LOCAL_TSIL.Init("lua.extraLibs.loi")
 include("lua.extraLibs.APIs.custom_bomb_flags")
@@ -66,13 +114,13 @@ include("lua.items.funny")
 include("lua.entities.player.SynergiesCallbacks")
 
 if StageAPI and StageAPI.Loaded then
-    StageAPI.AddPlayerGraphicsInfo(EdithRestored.Enums.PlayerType.EDITH, {
-        Name = "gfx/ui/boss/playername_Edith.png",
-        Portrait = "gfx/ui/boss/playerportrait_Edith_A.png",
-        NoShake = false
-    })
+	StageAPI.AddPlayerGraphicsInfo(EdithRestored.Enums.PlayerType.EDITH, {
+		Name = "gfx/ui/boss/playername_Edith.png",
+		Portrait = "gfx/ui/boss/playerportrait_Edith_A.png",
+		NoShake = false,
+	})
 
-    --[[StageAPI.AddPlayerGraphicsInfo(EdithRestored.Enums.PlayerType.EDITH_B, {
+	--[[StageAPI.AddPlayerGraphicsInfo(EdithRestored.Enums.PlayerType.EDITH_B, {
         Name = "gfx/ui/boss/playername_Edith.png",
         Portrait = "gfx/ui/boss/playerportrait_Edith_B.png",
         NoShake = false
