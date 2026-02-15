bd985700552f3899ca157d5d5638d9aa5d032b23
BrakeDude <lockbrake@gmail.com>
2026-02-15 22:31:43 +0300

    Lower volume for womb/scarred womb/utero sliding sound

--- lua/entities/player/main.lua
+++ lua/entities/player/main.lua
@@ -444,6 +444,7 @@ local function EdithGridMovement(player, data)
 				local bdType = EdithRestored.Room():GetBackdropType()
 				local chap4 = IsChap4()
 				local slideSound = chap4 and SoundEffect.SOUND_MEATY_DEATHS or EdithRestored.Enums.SFX.Edith.ROCK_SLIDE
+				local slideVolume = chap4 and 0.3 or 1
 				local HasWater = EdithRestored.Room():HasWater()
 				local variant = HasWater and EffectVariant.BIG_SPLASH or (
 					chap4 and EffectVariant.POOF02 or EdithRestored.Enums.Entities.CUSTOM_DUST_CLOUD.Variant
@@ -475,7 +476,7 @@ local function EdithGridMovement(player, data)
 					slideGFX.Color = (tableColorTarget[bdType] or tableColorTarget.Default)
 				end
 
-				sfx:Play(slideSound)
+				sfx:Play(slideSound, slideVolume)
 				EdithRestored.Game:ShakeScreen(1)
 			end
 
