f49088c7e016612ae7df9f571e2c0677bf1e264f
BrakeDude <lockbrake@gmail.com>
2026-03-11 12:12:29 +0300

    MinimapAPI stuff

--- lua/mod_compat/minimapapi/main.lua
+++ lua/mod_compat/minimapapi/main.lua
@@ -3,8 +3,10 @@ EdithRestored:AddModCompat("MinimapAPI", function()
 	local Pickups = Sprite()
 	Pickups:Load("gfx/ui/minimapitems/pickups_edith_icons.anm2", true)
 	MinimapAPI:AddIcon("SoulOfEdithIcon", Pickups, "CustomIcons", 0)
+	MinimapAPI:AddIcon("LithiumPillIcon", Pickups, "CustomIcons", 1)
+	MinimapAPI:AddIcon("LithiumPillHorseIcon", Pickups, "CustomIcons", 2)
 	MinimapAPI:AddPickup(
-		EdithRestored.Enums.Pickups.Cards.CARD_SOUL_EDITH,
+		"RuneSoulOfEdith",
 		"SoulOfEdithIcon",
 		5,
 		300,
@@ -12,5 +14,25 @@ EdithRestored:AddModCompat("MinimapAPI", function()
 		MinimapAPI.PickupNotCollected,
 		"runes",
 		11050
+	)
+	MinimapAPI:AddPickup(
+		"PillLithium",
+		"LithiumPillIcon",
+		5,
+		70,
+		EdithRestored.Enums.Pickups.Pills.PILL_LITHIUM,
+		MinimapAPI.PickupNotCollected,
+		"pills",
+		9001
+	)
+	MinimapAPI:AddPickup(
+		"PillLithiumHorse",
+		"LithiumPillHorseIcon",
+		5,
+		70,
+		EdithRestored.Enums.Pickups.Pills.PILL_HORSE_LITHIUM,
+		MinimapAPI.PickupNotCollected,
+		"pills",
+		9050
 	)
 end)
